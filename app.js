e773d1cfceb63d26ef705901ed302634939ed145
Melanyrc19 <melanyrc.19@gmail.com>
2025-09-20 17:44:22 -0300

    funcino editar y añadir mascota

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ let todasLasMascotas = [];
 const filtroNombre = document.querySelector("#seccionFiltro input"); // input del nombre
 const filtroRaza = document.querySelector("#selectRaza");
 const filtroSexo = document.querySelector("#selectSexo");
+let botonAñadirNav = document.querySelector("#botonAñadir")
+let botonParaAñadir = document.querySelector("#botonParaAñadir")
+let seccionAñadirMascotas = document.querySelector("#seccionAñadirMascota")
+
+let idMascotaEditando = null;
+
+const inputNombre = document.querySelector("#nombreMascota");
+const inputRaza = document.querySelector("#razaMascota");
+const inputSexo = document.querySelector("#sexoMascota");
+const inputAvatar = document.querySelector("#imagenMascota");
 
 async function obtenerUsuarios() {
   try {
@@ -12,7 +22,6 @@ async function obtenerUsuarios() {
     const datos = await response.json();
      todasLasMascotas = datos; 
     mostrarUsuariosEnDOM(datos);
-    actualizarSelectRazas();
   } catch (error) {
     console.error("Error al obtener usuarios:", error);
   }
@@ -25,8 +34,8 @@ function mostrarUsuariosEnDOM(datos) {
     return;
   }
   seccionCards.innerHTML = "";
-  datos.forEach(({ job, raza, country, name, Sexo, avatar, id }) => {
-    // const {job, country, name, avatar, id} = dato
+  datos.forEach(({ raza, name, Sexo, avatar, id }) => {
+   
 
     const userCard = document.createElement("div");
     userCard.className = "column is-4-table is-6-mobile mb-3";
@@ -40,13 +49,13 @@ function mostrarUsuariosEnDOM(datos) {
                 </figure>
             </div>
             <div class="tags is-centered mb-3">
-                <span class="tag is-warning is-light">${Sexo || "N/A"}</span>
+                <span class="tag is-warning is-light">${Sexo || "Desconocido"}</span>
                 <span class="tag is-success is-light">${raza || "Desconocida"}</span>
             </div>
 
          
           <footer class="card-footer">
-            <a href="#" onclick="editarUsuario('${id}')" class="button card-footer-item button is-small is-info">Editar</a>
+            <a href="#" id="botonParaEditar" onclick="editarMasc('${id}')" class="button card-footer-item button is-small is-info">Editar</a>
             <a href="#" onclick="eliminarUsuario('${id}')" class="button card-footer-item button is-small is-danger">Eliminar</a>
           </footer>
 
@@ -76,3 +85,110 @@ window.eliminarUsuario = async function (id) {
 
 obtenerUsuarios();
 
+
+
+
+
+botonAñadirNav.addEventListener("click", ()=>{
+seccionAñadirMascotas.classList.toggle("is-hidden")
+ document.querySelector("#tituloFormulario").textContent = "Añadir Mascota";
+})
+const formMascota = document.querySelector("#formMascota");
+
+
+formMascota.addEventListener("submit", async (e) => {
+  e.preventDefault(); 
+
+ const accion = btnSubmit.dataset.accion;  
+
+
+  try {
+    if (accion === "edit") {
+      const id = btnSubmit.dataset.id;
+      await editarMascotaApi(id);
+  } else {
+       await añadirMascotaApi();
+  }
+
+    obtenerUsuarios();
+
+    formMascota.reset();
+
+    seccionAñadirMascotas.classList.add("is-hidden");
+     document.querySelector("#btnSubmit").textContent = "Añadir";
+     tituloFormulario.textContent = "Añadir Mascota";
+   
+
+  } catch (error) {
+    console.error("Error al guardar:", error);
+    alert("No se pudo guardar la mascota");
+  }
+});
+
+window.editarMasc = async function(id){
+  try {
+    const response = await fetch(`${API_URL}/${id}`);
+    const mascota = await response.json();
+    inputNombre.value = mascota.name;
+    inputRaza.value = mascota.raza;
+    inputSexo.value = mascota.Sexo;
+    inputAvatar.value = mascota.avatar;
+    seccionAñadirMascotas.classList.remove("is-hidden");
+    tituloFormulario.textContent = "Editar mascota";
+    btnSubmit.textContent = "Guardar cambios";
+    btnSubmit.dataset.accion = "edit";
+    btnSubmit.dataset.id = id;
+  } catch (error) {
+    console.error(error);
+    alert("No se pudo cargar la mascota para editar");
+  }
+};
+ async function editarMascotaApi (id) {
+  try {
+     const mascotaEditada = {
+        name: inputNombre.value,
+        raza: inputRaza.value,
+        Sexo: inputSexo.value,
+        avatar: inputAvatar.value,
+    };
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(mascotaEditada),
+    });
+    if (!response.ok) throw new Error("Error al editar mascota");
+    
+    alert("Mascota editada correctamente ✅");
+    
+
+  } catch (error) {
+    console.error(error);
+    alert("No se pudo cargar la mascota para editar");
+  }
+};
+async function  añadirMascotaApi() {
+  try{
+     const nuevaMascota = {
+      name: document.querySelector("#nombreMascota").value,
+      raza: document.querySelector("#razaMascota").value,
+      Sexo: document.querySelector("#sexoMascota").value,
+      avatar: document.querySelector("#imagenMascota").value,
+    };
+      const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(nuevaMascota),
+    });
+     if (!response.ok) throw new Error("Error al añadir mascota");
+
+    alert("Mascota añadida correctamente 🎉");
+
+  } catch (error) {
+    console.error(error);
+    alert("No se pudo añadir la mascota");
+  }
+
+
+  
+  
+}
